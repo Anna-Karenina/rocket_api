3c3f07336f88dc61ad8e4edc406d9599268902d3
AnnaKarenina <svr_taile@gmail.com>
2023-03-04 13:31:03 +0300

    add Basic auth method

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,11 @@ extern crate rocket;
 use rocket::response::status;
 use rocket::serde::json::{json, Value};
 
+mod auth;
+use auth::BasicAuth;
+
 #[get("/rustaceans")]
-fn get_rustacens() -> Value {
+fn get_rustacens(_auth: BasicAuth) -> Value {
     json!([
     {"id": 1, "name": "John Doe"},
     {"id": 2, "name": "John Doe next"},
@@ -13,7 +16,7 @@ fn get_rustacens() -> Value {
 }
 
 #[get("/rustaceans/<id>")]
-fn view_rustacen(id: i32) -> Value {
+fn view_rustacen(id: i32, _auth: BasicAuth) -> Value {
     json!({
       "id": id,
       "name": "John Doe",
@@ -22,23 +25,28 @@ fn view_rustacen(id: i32) -> Value {
 }
 
 #[post("/rustaceans", format = "json")]
-fn create_rustacen() -> Value {
+fn create_rustacen(_auth: BasicAuth) -> Value {
     json!({"id": 3, "name": "John Doe third", "email":  "John_Doe@duck.com"})
 }
 
 #[put("/rustaceans/<id>", format = "json")]
-fn update_rustacen(id: i32) -> Value {
+fn update_rustacen(id: i32, _auth: BasicAuth) -> Value {
     json!({"id": id, "name": "John Doe", "email":  "John_Doe@duck.com"})
 }
 
 #[delete("/rustaceans/<_id>")]
-fn delete_rustacen(_id: i32) -> status::NoContent {
+fn delete_rustacen(_id: i32, _auth: BasicAuth) -> status::NoContent {
     status::NoContent
 }
 
 #[catch(404)]
 fn not_found() -> Value {
     json!("Not found")
+}
+
+#[catch(401)]
+fn unauthorized() -> Value {
+    json!("Unauthorized")
 }
 
 #[rocket::main]
@@ -54,7 +62,7 @@ async fn main() {
                 delete_rustacen,
             ],
         )
-        .register("/", catchers![not_found])
+        .register("/", catchers![not_found, unauthorized])
         .launch()
         .await;
 }
